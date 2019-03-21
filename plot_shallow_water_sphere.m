9a8b8b716d00558f8062161d4956333c287d85bf
Nothing <44038716+Houmura@users.noreply.github.com>
2019-03-21 16:51:14 +0000

    minor improvement of output
    > output the result as jpeg format
    > add the name of variable in the title and the colour bar
    > can make a series of images then can be merged into a video via ffmpeg

--- plot_shallow_water_sphere.m
+++ plot_shallow_water_sphere.m
@@ -1,6 +1,7 @@
 % an example plotting program with m_map
 plot1='vort';
 print1='no';
+print_series=true;
 
 figure('renderer','painters');%maxfigsize
 if viscous_dissipation == false
@@ -16,14 +17,16 @@ for i=1:p
     vorticity(2:end-1,2:end-1) = (u_save(2:end-1,1:end-2,i)-u_save(2:end-1,3:end,i)) ...
         + (v_save(3:end,2:end-1,i)-v_save(1:end-2,2:end-1,i));
 %    m_proj('robinson','long',[-180 180],'lat',[-90 90]);
-    m_proj('stereographic','lat',90,'lon',0,'radius',25)
+    m_proj('stereographic','lat',90,'lon',0,'radius',30)
 %    m_proj('ortho','long',180,'lat',80);
     hold off;
     switch plot1
         case 'h'
             F1=h_save([lon/2+1:lon 1:lon/2],:,i); 
+            colorbar_name="Height Field (m)";
         case 'vort'
             F1=vorticity([lon/2+1:lon 1:lon/2],:); 
+            colorbar_name="Vorticity (s^{-1})";
         otherwise
             disp(['error ']);
             return;
@@ -35,11 +38,20 @@ for i=1:p
 %     m_gshhs_c('color','k');
 %     m_quiver(PHI2(1:3:end,1:3:end)'.*180./pi,THETA(1:3:end,1:3:end)'.*180./pi,...
 %         u_save([91:3:180 1:3:90],1:3:end,i)',v_save([91:3:180 1:3:90],1:3:end,i)')
-    title({['Time (hrs): ',num2str(t_save(i)./3600)],
+    title({colorbar_name;['Time (hrs): ',num2str(t_save(i)./3600)],
         [num2str(i),' of ',num2str(p),'; viscosity: ',num2str(vis)]});
-    colorbar('horiz')
+    object_colorbar = colorbar;    %return the Colorbar object and use this object to set properties after creating the colorbar
+    object_colorbar.Label.String = colorbar_name;
 
-    pause; %(0.1);
+    
+%   OUTPUT METHODS
+    if print_series == false
+        pause; %(0.1);
+    else
+        saveas(gcf,[num2str(i),'.jpg'],'jpeg')
+        hold off
+    end
+    
     
     if strcmp(print1,'yes')
         eval(['print -dpng -r25 aframe',num2str(i,'%03d'),'.png']);
